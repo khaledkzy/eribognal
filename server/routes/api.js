378138b95632716ae124067c7c72afcfcca2134d
Unknown <khaledkzy@gmail.com>
2017-10-30 22:43:49 +0000

    Refactor the mongoose.connect function from all the files

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const connection = require('../dbClients/connection')
 const Place = require('../models/Place');
-const mongoose = require('mongoose')
-mongoose.Promise = global.Promise;
 const placesDB = require('../dbClients/placesDB')
 
 
@@ -21,20 +18,10 @@ router.get('/questions', function (req, res, next) {
 });
 
 router.get('/places', function (req, res, next) {
-    mongoose.connect(connection);
     Place.find({}, (error, data) => {
         res.json(data)
     })
 })
-router.get('/places', function (req, res, next) {
-    const callBack = (error, places) => {
-        if (error) {
-            res.sendStatus(500);
-        } else {
-            res.json(places)
-        }
-    };
-    placesDB.getPlaces(callBack)
-});
+
 
 module.exports = router;
